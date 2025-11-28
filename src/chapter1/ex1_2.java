9476b26a813d4445a4f37b40fe61a65e8bfd1a48
few717254 <few717254@gmail.com>
2025-11-28 10:50:32 +0700

    Implements user input functionality in ex2 and adds ex1_1 and ex1_2 classes for demonstration

---
+++ src/chapter1/ex1_2.java
@@ -0,0 +1,9 @@
+package chapter1;
+
+public class ex1_2 {
+    public static void main(String[] args) {
+        System.out.println("Beginner");
+        System.out.println("Java");
+        System.out.println("PRogramming!");
+    }
+}
--- src/chapter1/ex2.java
+++ src/chapter1/ex2.java
@@ -1,7 +1,31 @@
+// package chapter1;
+
+// public class ex2 {
+//     public static void main(String[] args) {
+//         System.out.println("ID : 68-060216-1203-7\nName : Paphavin\nSurname : Tithichunhakun\nGender : Male\nAge : 18");
+//     }
+// }
+
 package chapter1;
 
+import javax.swing.JOptionPane;
+import javax.swing.JTextArea;
+
 public class ex2 {
+
     public static void main(String[] args) {
-        System.out.println("ID : 68-060216-1203-7\nName : Paphavin\nSurname : Tithichunhakun\nGender : Male\nAge : 18");
+        JTextArea outputArea = new JTextArea();
+        String output = "";
+        String id = JOptionPane.showInputDialog("Enter Id: ");
+        String name = JOptionPane.showInputDialog("Enter Name: ");
+        String surname = JOptionPane.showInputDialog("Enter Surname");
+        String gender = JOptionPane.showInputDialog("Enter Gender:");
+        int age = Integer.parseInt(JOptionPane.showInputDialog("Enter Age:"));
+        String room = JOptionPane.showInputDialog("Enter Room:");
+        output += "Id : " + id + "\nName : " + name + "\nSurname : " + surname + "\nGender: " + gender + "\nAge: " + age
+                + "\nRoom: " + room;
+        outputArea.setText(output);
+        JOptionPane.showMessageDialog(null, output);
+
     }
 }
