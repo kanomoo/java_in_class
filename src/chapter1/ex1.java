051db809b33c01b3ddb1613eb6e0f5fdf626176c
Your Name <your.email@example.com>
2025-11-28 20:46:54 +0700

    Removes commented-out code for cleaner implementation in ex1.java

--- src/chapter1/ex1.java
+++ src/chapter1/ex1.java
@@ -5,3 +5,19 @@ public class ex1 {
         System.out.println("     ee  ee\n    ff  ff\n   Co  Co\n###############\n#  hello  #   #\n#  Java   ####\n#         #\n #########");
     }
 }
+
+
+// package chapter1;
+
+// import javax.swing.JOptionPane;
+// import javax.swing.JTextArea;
+
+// public class ex1 {
+
+//     public static void main(String[] args) {
+//         JTextArea outputArea = new JTextArea();
+//         String output = ("     ee  ee\n    ff  ff\n   Co  Co\n###############\n#  hello  #   #\n#  Java   ####\n#         #\n #########");
+//         outputArea.setText(output);
+//         JOptionPane.showMessageDialog(null, outputArea);
+//     }
+// }
