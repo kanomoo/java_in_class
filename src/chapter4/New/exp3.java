f98b947ffe4a52b6738da254822d3d734bf95916
Your Name <your.email@example.com>
2025-12-22 12:30:23 +0700

    Add exp3 class with user input functionality; update launch configuration for new class

--- src/chapter4/New/exp3.java
+++ src/chapter4/New/exp3.java
@@ -5,11 +5,24 @@ import java.util.Scanner;
 public class exp3 {
     public void startMenu() {
         Scanner scanner = new Scanner(System.in);
+        int line = 0;
         System.out.print("Input number line : ");
-        int line = scanner.nextInt();
+        line = scanner.nextInt();
         System.out.print("Input character : ");
         String ch = scanner.next();
         System.out.print("Input type : ");
         int type = scanner.nextInt();
+        System.out.println();
+        if(type == 1)
+            for(int n = 1; n <= 5; n++)
+                System.out.println(ch.repeat(n));
+        else if (type == 2)
+            for(int n = line; n >= 1; n--)
+                System.out.println(ch.repeat(n));
+        scanner.close();
+    }
+
+    public static void main(String[] args) {
+        new exp3().startMenu();
     }
 }
