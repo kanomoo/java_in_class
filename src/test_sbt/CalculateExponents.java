8f7797109af4cb88f28dffbcdc19f46293664c81
Your Name <your.email@example.com>
2026-01-08 15:27:31 +0700

    Refactor CalculateExponents class to enhance GUI layout and functionality

--- src/test_sbt/CalculateExponents.java
+++ src/test_sbt/CalculateExponents.java
@@ -1,9 +1,11 @@
 package test_sbt;
 
 import java.awt.Container;
+import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -11,28 +13,41 @@ import javax.swing.JTextField;
 
 public class CalculateExponents extends JFrame implements ActionListener {
     Container container;
-    JPanel panelText, panelBtn;
+    JPanel panelBtn;
     JTextField textBase, textExponent;
+    JButton calBtn, clearBtn;
 
     public CalculateExponents () {
         super("Calculate exponents.");
         container = getContentPane();
-        setSize(400,400);
+        container.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 10));
+        initGui();
+        setSize(250,400);
         setVisible(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
     public void initGui() {
+        container.add(new JLabel("Base"));
+        textBase = new JTextField(20);
+        container.add(textBase);
+        container.add(new JLabel("Exponent"));
+        textExponent = new JTextField(20);
+        container.add(textExponent);
+        initPanelBtn();
         
     }
 
-    public void initPanelText() {
-        container.add(new JLabel("Base"));
-
-    }
-
     public void initPanelBtn() {
-
+        panelBtn = new JPanel();
+        panelBtn.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 10));
+        calBtn = new JButton("calculate");
+        calBtn.addActionListener(this);
+        panelBtn.add(calBtn);
+        clearBtn = new JButton("Clear results");
+        clearBtn.addActionListener(this);;
+        panelBtn.add(clearBtn);
+        container.add(panelBtn);
     }
 
     @Override
