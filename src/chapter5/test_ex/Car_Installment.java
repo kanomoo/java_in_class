51bcf9873d6b056fbbc6ef7d4bf7c2b0ab9eef1e
kanomoo <few717254@gmail.com>
2026-01-12 22:44:51 +0700

    Add launch configuration for Car_Installment class and update user input to use JOptionPane

--- src/chapter5/test_ex/Car_Installment.java
+++ src/chapter5/test_ex/Car_Installment.java
@@ -161,10 +161,89 @@
 
 
 
+
+
+
+
+
+// package chapter5.test_ex;
+
+// import java.text.DecimalFormat;
+// import java.util.Scanner;
+
+// public class Car_Installment {
+//     DecimalFormat twoDigit = new DecimalFormat("###,###,##0.00");
+//     Scanner scanner = new Scanner(System.in);
+//     public double price, downPayment, interestRate;
+//     public double amDownPayment, amFinance, amInterest, amNetFinance, amInstallment;
+//     public int month;
+
+//     public void start() {
+//         while (true) {
+//             String output = ">>>   Main Menu   <<<";
+//             output += "=====================\n 0. Exit Program\n 1. Input Car Price (" + twoDigit.format(price) + ")";
+//             output += "\n 2. Input Down Payment (" + twoDigit.format(downPayment) + "%)";
+//             output += "\n 3. Input Interest Rate [Per Year] (" + twoDigit.format(interestRate) + "%)";
+//             output += "\n 4. Input Month Number(" + month + ")";
+//             output += "\n 5. Display Installment";
+//             output += "\n 6. Clear All data";
+//             output += "\nSelect choice : ";
+//             System.out.print(output);
+//             String choice = scanner.next().trim();
+//             switch (choice) {
+//                 case "0":
+//                     System.out.println("Exit Program..."); System.exit(0); break;
+//                 case "1":
+//                     System.out.print("Enter car price : "); price = (scanner.nextDouble()); break;
+//                 case "2":
+//                     System.out.print("Enter down payment(%) : "); downPayment = (scanner.nextDouble()); break;
+//                 case "3":
+//                     System.out.print("Enter interest rate(%) per year : "); interestRate = (scanner.nextDouble()); break;
+//                 case "4":
+//                     System.out.print("Enter month : "); month = (scanner.nextInt()); break;
+//                 case "5":
+//                     showInstallment(); break;
+//                 case "6":
+//                     clearData(); break;
+//                 default:
+//                     System.out.println("No choice"); break;
+//             }
+//             System.out.println();
+//         }
+//     }
+
+//     public void showInstallment() {
+//         String output = "\nPrice car : " + twoDigit.format(price);
+//         output += "\nAmount down payment(" + twoDigit.format(downPayment) + "%) : " + twoDigit.format(amDownPayment = price * (downPayment / 100.0));
+//         output += "\nAmount finance : " + twoDigit.format(amFinance = price - amDownPayment);
+//         output += "\nAmount interest(" + twoDigit.format(interestRate) + ") : " + twoDigit.format(amInterest = amFinance * ((interestRate / 12.0) / 100.0) * (double) month);
+//         output += "\nAmount net finance : " + twoDigit.format(amNetFinance = amFinance + amInterest);
+//         output += "\nAmount installment(per month) : " + twoDigit.format(amInstallment = amNetFinance / (double) month);
+//         System.out.println(output);
+//     }
+
+//     public void clearData() {
+//         price = downPayment = interestRate  =amDownPayment = amFinance = amInterest = amNetFinance = amInstallment  = month = 0;
+//     }
+
+//     public static void main(String[] args) {
+//         Car_Installment car = new Car_Installment();
+//         car.start();
+//     }
+// }
+
+
+
+
+
+
+
+
 package chapter5.test_ex;
 
 import java.text.DecimalFormat;
 import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 public class Car_Installment {
     DecimalFormat twoDigit = new DecimalFormat("###,###,##0.00");
@@ -176,43 +255,34 @@ public class Car_Installment {
     public void start() {
         while (true) {
             String output = ">>>   Main Menu   <<<";
-            output += "=====================\n 0. Exit Program\n 1. Input Car Price (" + twoDigit.format(price) + ")";
+            output += "\n=====================\n 0. Exit Program\n 1. Input Car Price (" + twoDigit.format(price) + ")";
             output += "\n 2. Input Down Payment (" + twoDigit.format(downPayment) + "%)";
             output += "\n 3. Input Interest Rate [Per Year] (" + twoDigit.format(interestRate) + "%)";
             output += "\n 4. Input Month Number(" + month + ")";
             output += "\n 5. Display Installment";
             output += "\n 6. Clear All data";
             output += "\nSelect choice : ";
-            System.out.print(output);
-            String choice = scanner.next().trim();
-            switch (choice) {
-                case "0":
-                    System.out.println("Exit Program..."); System.exit(0); break;
-
-                case "1":
-                    System.out.print("Enter car price : "); price = (scanner.nextDouble()); break;
-
-                case "2":
-                    System.out.print("Enter down payment(%) : "); downPayment = (scanner.nextDouble()); break;
-
-                case "3":
-                    System.out.print("Enter interest rate(%) per year : "); interestRate = (scanner.nextDouble()); break;
-
-                case "4":
-                    System.out.print("Enter month : "); month = (scanner.nextInt()); break;
-
-                case "5":
-                    showInstallment(); break;
-
-                case "6":
-                    clearData(); break;
-
-                default:
-                    System.out.println("No choice"); break;
+            String choice = JOptionPane.showInputDialog(null, output);
+            if (choice.equals("0")) {
+                JOptionPane.showMessageDialog(null, "Exit program...", "Error Message", JOptionPane.ERROR_MESSAGE);
+                System.exit(0);
             }
-            System.out.println();
+            else if (choice.equals("1"))
+                price = Double.parseDouble(JOptionPane.showInputDialog(null, "Enter car price : "));
+            else if (choice.equals("2"))
+                downPayment = Double.parseDouble(JOptionPane.showInputDialog(null, "Enter down payment(%) : "));
+            else if (choice.equals("3"))
+                interestRate = Double.parseDouble(JOptionPane.showInputDialog(null, "Enter interest rate(%) per year : "));
+            else if (choice.equals("4"))
+                month = Integer.parseInt(JOptionPane.showInputDialog(null, "Enter month : "));
+            else if (choice.equals("5"))
+                showInstallment();
+            else if (choice.equals("6"))
+                clearData();
+            else
+                JOptionPane.showMessageDialog(null, "No choice", "Warning Message", JOptionPane.WARNING_MESSAGE);
+            }
         }
-    }
 
     public void showInstallment() {
         String output = "\nPrice car : " + twoDigit.format(price);
@@ -221,11 +291,11 @@ public class Car_Installment {
         output += "\nAmount interest(" + twoDigit.format(interestRate) + ") : " + twoDigit.format(amInterest = amFinance * ((interestRate / 12.0) / 100.0) * (double) month);
         output += "\nAmount net finance : " + twoDigit.format(amNetFinance = amFinance + amInterest);
         output += "\nAmount installment(per month) : " + twoDigit.format(amInstallment = amNetFinance / (double) month);
-        System.out.println(output);
+        JOptionPane.showMessageDialog(null, output);
     }
 
     public void clearData() {
-        price = downPayment = interestRate  =amDownPayment = amFinance = amInterest = amNetFinance = amInstallment  = month = 0;
+        price = downPayment = interestRate = amDownPayment = amFinance = amInterest = amNetFinance = amInstallment  = month = 0;
     }
 
     public static void main(String[] args) {
