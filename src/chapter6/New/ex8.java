a7be208c08238ed60ae8cb98d753ddef3b8520bb
Your Name <your.email@example.com>
2026-02-06 22:25:00 +0700

    Refactor ex8 class for improved structure and functionality; update bubble sort and binary search methods, enhance user interaction, and streamline main program execution

--- src/chapter6/New/ex8.java
+++ src/chapter6/New/ex8.java
@@ -82,80 +82,160 @@
 
 // }
 
+
+
+
+
+
+
+// package chapter6.New;
+
+// import java.text.DecimalFormat;
+// import java.util.Random;
+
+// import javax.swing.JOptionPane;
+
+// public class ex8 {
+//     DecimalFormat twoDigits = new DecimalFormat("00");
+//     Random rnd = new Random();
+//     int[] array = new int[30];
+//     String output;
+
+
+//     public ex8() {
+//         for (int i = 0; i < array.length; i++) array[i] = rnd.nextInt(101);
+//         bubbleSort(array);
+//         while (true) {
+//             String num = JOptionPane.showInputDialog("Enter number to search(-1 : exit)");
+//             if (num == null) {JOptionPane.showMessageDialog(null, "Exit Program..", "Message", JOptionPane.INFORMATION_MESSAGE); System.exit(0);}
+//             else if (num.equals("-1")) System.exit(0);
+//             else binarySearch(Integer.parseInt(num));
+//         }
+//     }
+
+
+//     public void bubbleSort(int array[]) {
+//         boolean done = true;
+//         for (int l = 1; l < array.length; l++) {
+//             done = true;
+//             for (int i = 0; i < array.length - 1; i++) {
+//                 if (array[i] > array[i + 1]) {
+//                     swap(array, i, i + 1);
+//                     done = false;
+//                 }
+//             }
+//             if (done)
+//                 break;
+//         }
+//     }
+
+//     public void swap(int array[], int first, int second) {
+//         int hold = array[first];
+//         array[first] = array[second];
+//         array[second] = hold;
+//     }
+
+//     public void binarySearch(int num) {
+//         int low = 0, height = array.length, mid = (low + height) / 2;
+//         output = "Potions of array searched\n";
+//         while (low <= height) {
+//             buildOutput(low, mid, height, num);
+//             if (num > array[mid]) low = mid + 1;
+//             else if (num < array[mid]) height = mid - 1;
+//             else break;
+//             mid = (low + height) / 2;
+//         }
+//         output += (num == array[mid]) ? "\nValue = " + array[mid] : "\nValue not found";
+//         JOptionPane.showMessageDialog(null, output);
+
+//     }
+
+//     public void buildOutput(int low, int mid, int height, int num) {
+//         for (int i = 0; i < array.length; i++) {
+//             if (i == mid) output += array[i] + "* ";
+//             else if (i >= low && i <= height) output += array[i] + "  ";
+//             else output += "    ";
+//         }
+//         output += "\n";
+
+//     }
+
+//     public static void main(String[] args) {
+//         new ex8();
+//     }
+// }
+
+
+
+
+
+
+
+
 package chapter6.New;
 
-import java.text.DecimalFormat;
 import java.util.Random;
 
 import javax.swing.JOptionPane;
 
 public class ex8 {
-    DecimalFormat twoDigits = new DecimalFormat("00");
+    int array[] = new int[30];
     Random rnd = new Random();
-    int[] array = new int[30];
-    String output;
+    String num, output;
 
-
-    public ex8() {
-        for (int i = 0; i < array.length; i++) array[i] = rnd.nextInt(101);
-        bubbleSort(array);
+    public void MainProgram() {
+        for (int i = 0; i < array.length; i++) array[i] = rnd.nextInt(100);
+        bubbleSort();
+        for (int l : array) System.out.print(l + "  ");
         while (true) {
-            String num = JOptionPane.showInputDialog("Enter number to search(-1 : exit)");
-            if (num == null) {JOptionPane.showMessageDialog(null, "Exit Program..", "Message", JOptionPane.INFORMATION_MESSAGE); System.exit(0);}
-            else if (num.equals("-1")) System.exit(0);
-            else binarySearch(Integer.parseInt(num));
+            num = JOptionPane.showInputDialog("Enter number binary search(-1 : Exit): ");
+            if (num == null) {JOptionPane.showMessageDialog(null, "Exit Program..."); System.exit(0);}
+            else if (num.equals("-1")) {JOptionPane.showMessageDialog(null, "Exit Program..."); System.exit(0);}
+            else {binarySearch(Integer.parseInt(num));}
         }
     }
 
-
-    public void bubbleSort(int array[]) {
+    public void bubbleSort() {
         boolean done = true;
         for (int l = 1; l < array.length; l++) {
             done = true;
             for (int i = 0; i < array.length - 1; i++) {
-                if (array[i] > array[i + 1]) {
-                    swap(array, i, i + 1);
-                    done = false;
-                }
+                if (array[i] > array[i + 1]) swap(i, i + 1);
+                done = false;
             }
-            if (done)
-                break;
+            if (done) break;
         }
     }
 
-    public void swap(int array[], int first, int second) {
+    public void swap(int first, int second) {
         int hold = array[first];
         array[first] = array[second];
         array[second] = hold;
     }
 
     public void binarySearch(int num) {
-        int low = 0, height = array.length, mid = (low + height) / 2;
-        output = "Potions of array searched\n";
+        int low = 0, height = array.length - 1, mid = (low + height) / 2;
+        output = "Binary search\n";
         while (low <= height) {
-            buildOutput(low, mid, height, num);
+            buildOutput(low, height, mid, num);
             if (num > array[mid]) low = mid + 1;
             else if (num < array[mid]) height = mid - 1;
             else break;
             mid = (low + height) / 2;
-            // System.out.println(low + " " + height);
         }
-        output += (num == array[mid]) ? "\nValue = " + array[mid] : "\nValue not found";
-        JOptionPane.showMessageDialog(null, output);
-
+        JOptionPane.showMessageDialog(null, output += (array[mid] == num) ? "Value = " + array[mid] : "Value not found");
     }
 
-    public void buildOutput(int low, int mid, int height, int num) {
+    public void buildOutput(int low, int height, int mid, int num) {
         for (int i = 0; i < array.length; i++) {
             if (i == mid) output += array[i] + "* ";
             else if (i >= low && i <= height) output += array[i] + "  ";
             else output += "    ";
         }
         output += "\n";
-
     }
 
     public static void main(String[] args) {
-        new ex8();
+        new ex8().MainProgram();
     }
 }
