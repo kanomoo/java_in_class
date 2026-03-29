c587edaad44e3ca0a5f7e3257be9877675a041bc
Your Name <your.email@example.com>
2026-03-29 15:15:22 +0700

    Remove Test.java and its compiled class; comment out code for future reference

--- src/testF/Test.java
+++ src/testF/Test.java
@@ -1,15 +1,15 @@
-package testF;
+// package testF;
 
-import java.io.FileReader;
+// import java.io.FileReader;
 
-import javax.swing.JOptionPane;
+// import javax.swing.JOptionPane;
 
-public class Test {
-    public static void main(String[] args) {
-        JOptionPane.showMessageDialog(null, args);
-        FileReader reader = new FileReader(null);
-        reader.readAllLines();
-        Double.parseDouble()
-    }
-}
+// public class Test {
+//     public static void main(String[] args) {
+//         JOptionPane.showMessageDialog(null, args);
+//         FileReader reader = new FileReader(null);
+//         reader.readAllLines();
+//         Double.parseDouble()
+//     }
+// }
 
