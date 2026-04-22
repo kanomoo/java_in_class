fca27da84a20b59c6eea97610360a9bff532a4d8
kanomoo <few717254@gmail.com>
2026-04-23 02:05:17 +0700

    Add PositiveNegativeZero and SpeedConverter classes with main methods and conversion logic

--- src/Udemy/PositiveNegativeZero.java
+++ src/Udemy/PositiveNegativeZero.java
@@ -1,4 +1,15 @@
 package Udemy;
 
 public class PositiveNegativeZero {
+    public static void main(String[] args) {
+        checkNumber(0);
+    }
+
+    public static void checkNumber(int number) {
+        String result = "";
+        if (number > 0) result = "positive";
+        else if (number < 0) result = "negative";
+        else result = "zero";
+        System.out.println(result);
+    }
 }
--- src/Udemy/SpeedConverter.java
+++ src/Udemy/SpeedConverter.java
@@ -1,4 +1,21 @@
 package Udemy;
 
 public class SpeedConverter {
+
+    public static void main(String[] args) {
+        printConversion(95.75);
+    }
+
+    public static double toMilesPerHour(double kilometersPerHour){
+        if (kilometersPerHour < 0) return -1L;
+//        return Math.round(kilometersPerHour * 0.621371D);
+        return (kilometersPerHour * 0.621371D);
+
+
+    }
+
+    public static void printConversion(double kilometersPerHour){
+        if (kilometersPerHour < 0) System.out.println("Invalid Value");
+        else System.out.println(kilometersPerHour + " km/h = " + toMilesPerHour(kilometersPerHour) + " mi/h");
+    }
 }
